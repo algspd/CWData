b3701af4574544bd6a563f97f206287bbd75d582
algspd <root@ip-172-31-35-136.us-west-2.compute.internal>
2013-09-27 16:39:20 +0000

    Added some printers, now success page links to the tree and add another printer

--- src/www/application/views/cw_success.php
+++ src/www/application/views/cw_success.php
@@ -17,6 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="sobre_impresora">
 <h3>Impresora insertada correctamente</h3>
 La impresora se ha insertado en la base de datos.
+Ahora puedes <a href="/">visitar el arbol</a><br/>o <a href="/index.php/cw">insertar otra impresora</a>.
 </div>
 
 
